65ce6989e95a7a7d26b4d8ddeb2ec130df947335
Elisio Quintino <elife599@student.liu.se>
2021-09-26 17:06:43 +0200

    commit prior to build configuration, just in case

---
+++ R/temperatuR-package.R
@@ -0,0 +1,8 @@
+#'@details
+#'
+#'This package is a non-official wrapper for the Temperatur.nu API.
+#'More about the API you can find in https://www.temperatur.nu/info/api/
+#'More about the project you can find in https://www.temperatur.nu/info/
+#'
+#'@keywords internal
+"_PACKAGE"
